57290ed9d1aadf10553bce96ff81546f15cfc01c
Muneef Marzook <muneef.dev@gmail.com>
2025-07-05 08:20:27 +0530

    Setup api and express

---
+++ api/src/index.ts
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+import express from 'express';
+
+dotenv.config();
+
+const port = 3000;
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
