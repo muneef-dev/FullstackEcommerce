8cb2316b22a65e03c3803c2fe6a63d50418ddd77
Muneef Marzook <muneef.dev@gmail.com>
2025-07-07 21:46:25 +0530

    Drizzle setup:

---
+++ api/drizzle.config.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  out: './drizzle',
+  schema: ['./src/db/productSchema.ts'],
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+  verbose: true, // it will show more logs about whats happaning
+  strict: true, // which will enforece more rules
+});
