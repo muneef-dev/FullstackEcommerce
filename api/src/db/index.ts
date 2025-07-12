974c8a041e274aefda54af345240f9a6e623b5ee
Muneef Marzook <muneef.dev@gmail.com>
2025-07-12 07:12:56 +0530

    Neon db added

--- api/src/db/index.ts
+++ api/src/db/index.ts
@@ -2,8 +2,13 @@ import 'dotenv/config';
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 
+const isProduction = process.env.NODE_ENV === 'production';
+
+// Initialize database connection
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL!,
+  connectionString: isProduction 
+    ? process.env.NEON_DATABASE_URL
+    : process.env.DATABASE_URL,
 });
 
 export const db = drizzle({ client: pool });
